c1d73b687ef4cb65ec880da897c053c86f714082
이응민 <ppoppo9802@naver.com>
2025-04-17 03:59:15 +0900

    Part 01 Day 07

---
+++ Part1/UnrealBuildSystem/Plugins/GameUtility/Source/CommonUtility/Private/CommonUtility.cpp
@@ -0,0 +1,5 @@
+﻿
+#include "CommonUtility.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FDefaultModuleImpl, CommonUtility);
---
+++ Part1/UnrealBuildSystem/Source/UnrealBuildSystem/UnrealBuildSystem.cpp
@@ -0,0 +1,5 @@
+﻿
+#include "UnrealBuildSystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, UnrealBuildSystem, "UnrealBuildSystem");
